8139089623766be7e26cd981be0dd1bad0a94cf2
Pau Botargues Julio <pbotargu@car9s6.42barcelona.com>
2024-05-15 12:37:33 +0200

    clean function redirections:

--- src/redirections/redirections.c
+++ src/redirections/redirections.c
@@ -6,7 +6,7 @@
 /*   By: pbotargu <pbotargu@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 12:31:00 by pbotargu          #+#    #+#             */
-/*   Updated: 2024/05/15 12:36:29 by pbotargu         ###   ########.fr       */
+/*   Updated: 2024/05/15 12:37:13 by pbotargu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,38 +30,4 @@ int     redirections(t_token **tokens, t_list **env, t_list **export, t_executor
         temp = temp->next;
     }
     return (0);
-}
-
-int	builtins(t_token **tokens, t_list **export, t_list **env)
-{
-	t_token	**temp;
-	int		i;
-
-	i = 0;
-	temp = tokens;
-	while ((*temp))
-	{
-		if (ft_strcmp((*temp)->wrd, "echo"))
-		{
-			i = ft_echo(tokens);
-			return (i);
-		}
-		if (ft_strcmp((*temp)->wrd, "env"))
-			i = ft_env(env);
-		if (ft_strcmp((*temp)->wrd, "pwd"))
-			i = ft_pwd();
-		if (ft_strcmp((*temp)->wrd, "exit"))
-			ft_exit(tokens);
-		if (ft_strcmp((*temp)->wrd, "export"))
-			ft_export(tokens, export, env);
-		if (ft_strcmp((*temp)->wrd, "cd"))
-			i = ft_cd(tokens, export, env);
-		if (ft_strcmp((*temp)->wrd, "unset"))
-		{
-			ft_unset(export, (*temp)->next->wrd);
-			ft_unset(env, (*temp)->next->wrd);
-		}
-		(*temp) = (*temp)->next;
-	}
-	return (i);
 }
