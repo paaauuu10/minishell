03fbb58be3704bfbdc2a57ddebed5e3fe92c3a0a
Pau Botargues Julio <pbotargu@car1s5.42barcelona.com>
2024-05-16 11:48:30 +0200

    nothing

--- src/redirections/redirections.c
+++ src/redirections/redirections.c
@@ -6,7 +6,7 @@
 /*   By: pbotargu <pbotargu@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 12:31:00 by pbotargu          #+#    #+#             */
-/*   Updated: 2024/05/15 14:26:11 by pbotargu         ###   ########.fr       */
+/*   Updated: 2024/05/15 14:41:43 by pbotargu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,20 @@
 int ft_output_redirect(t_token **tokens, t_list **env, t_list **export, t_executor *t_exec)
 {
     int file;
+    int save_file;
     t_token *temp;
     
+    save_file = dup(STDOUT_FILENO);
     temp = *tokens;
     file = open(temp->next->next->next->wrd, O_WRONLY | O_CREAT, 0777);
     if (file == -1 )
         return (0); /*handle error*/
-    dup2(file, 1);
+    dup2(file, STDOUT_FILENO);
     builtins(tokens, env, export);
     ft_exec(tokens, env, t_exec);
+    close(file);
+    dup2(save_file, STDOUT_FILENO);
+    close(save_file);
     return (0);
 }
 
