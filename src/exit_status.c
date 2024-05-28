404bc83c06e022281274047afbd4071ed43e9766
Legolas228 <pauborrull8@gmail.com>
2024-05-28 14:47:30 +0200

    real merge

--- src/exit_status.c
+++ src/exit_status.c
@@ -5,49 +5,18 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: pborrull <pborrull@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-<<<<<<< HEAD:src/exit_status.c
 /*   Created: 2024/05/21 14:14:15 by pborrull          #+#    #+#             */
 /*   Updated: 2024/05/21 14:31:46 by pborrull         ###   ########.fr       */
-=======
-/*   Created: 2024/03/14 12:37:35 by pborrull          #+#    #+#             */
-/*   Updated: 2024/04/23 14:40:20 by pborrull         ###   ########.fr       */
->>>>>>> 8b73d5e7bf7b0d94a37c6b66c27535a9d5bbf752:blt_env.c
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-<<<<<<< HEAD:src/exit_status.c
 char	*ft_exit_status(int i, int j)
-=======
-int	ft_env(t_token	**env)
-{
-	int		i;
-	t_token	*temp;
-
-	temp = *env;
-	i = 0;
-	while (temp->next)
-	{
-		printf("%s\n", temp->wrd);
-		temp = temp->next;
-	}
-	return (0);
-}
-/*int	ft_env(char	**env)
->>>>>>> 8b73d5e7bf7b0d94a37c6b66c27535a9d5bbf752:blt_env.c
 {
 	static char	*k;
 
-<<<<<<< HEAD:src/exit_status.c
 	if (j == 1)
 		k = ft_itoa(i);
 	return (k);
 }
-=======
-	i = 0;
-	while (env[i])
-		printf("%s\n", env[i++]);
-	return (0);
-}*/
->>>>>>> 8b73d5e7bf7b0d94a37c6b66c27535a9d5bbf752:blt_env.c
--- src/main.c
+++ src/main.c
@@ -6,64 +6,22 @@
 /*   By: pborrull <pborrull@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 09:13:31 by pborrull          #+#    #+#             */
-<<<<<<< HEAD:src/main.c
 /*   Updated: 2024/05/22 14:16:13 by pborrull         ###   ########.fr       */
-=======
-/*   Updated: 2024/04/24 14:22:49 by pborrull         ###   ########.fr       */
->>>>>>> 8b73d5e7bf7b0d94a37c6b66c27535a9d5bbf752:main.c
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-<<<<<<< HEAD:src/main.c
 void	free_tokens(t_token **tokens)
-=======
-t_token	**env_list(char **envp)
-{
-	t_token	**env;
-	int		i;
-
-	i = 0;
-	env = (t_token **)malloc(sizeof(t_token *));
-	while (envp[i])
-		add_token(env, new_token(envp[i++]));
-	return (env);
-}
-
-void	builtins(t_token **tokens,t_token **export, t_token **env)
->>>>>>> 8b73d5e7bf7b0d94a37c6b66c27535a9d5bbf752:main.c
 {
 	t_token	*temp;
 
 	while (*tokens)
 	{
-<<<<<<< HEAD:src/main.c
 		temp = (*tokens)->next;
 		free((*tokens)->wrd);
 		free(*tokens);
 		*tokens = temp;
-=======
-		if (ft_strcmp((*temp)->wrd, "echo"))
-		{
-			ft_echo(tokens);
-			return ;
-		}
-		if (ft_strcmp((*temp)->wrd, "env"))
-			ft_env(env);
-		if (ft_strcmp((*temp)->wrd, "pwd"))
-			ft_pwd();
-		if (ft_strcmp((*temp)->wrd, "exit"))
-			ft_exit(tokens);
-		if (ft_strcmp((*temp)->wrd, "export"))
-			ft_export(tokens, export, env);
-		if (ft_strcmp((*temp)->wrd, "unset"))
-		{
-			ft_unset(export, (*temp)->next->wrd);
-			ft_unset(env, (*temp)->next->wrd);
-		}
-		(*temp) = (*temp)->next;
->>>>>>> 8b73d5e7bf7b0d94a37c6b66c27535a9d5bbf752:main.c
 	}
 	*tokens = NULL;
 }
@@ -82,24 +40,11 @@ static void	error_checker(int argc, char **argv, char **envp)
 	}	
 	argv = NULL;
 }
-void	free_tokens(t_token **tokens)
-{
-	t_token	*temp;
 
-	while (*tokens)
-	{
-		temp = (*tokens)->next;
-		free((*tokens)->wrd);
-		free(*tokens);
-		*tokens = temp;
-	}
-	*tokens = NULL;
-}
 int	main(int argc, char **argv, char **envp)
 {
 	const char	*s;
 	t_token		**tokens;
-<<<<<<< HEAD:src/main.c
 	t_list		**env;
 	t_list		**export;
 
@@ -109,19 +54,6 @@ int	main(int argc, char **argv, char **envp)
 	//	exit(1);
 	signals();
 	ft_exit_status(0, 1);
-=======
-	t_token		**env;
-	t_token		**export;
-	
-	tokens = (t_token **)malloc(sizeof(t_token *));
-	argv = NULL;
-	if (argc != 1)
-	{
-		perror("Bad args\n");
-		exit(0);
-	}
-	signals();
->>>>>>> 8b73d5e7bf7b0d94a37c6b66c27535a9d5bbf752:main.c
 	env = env_list(envp);
 	export = env_list(envp);
 	while (1)
@@ -135,21 +67,12 @@ int	main(int argc, char **argv, char **envp)
 			printf("exit\n");
 			exit(1);
 		}
-<<<<<<< HEAD:src/main.c
 		ft_quote_error(s);
 		tokens = get_tok(tokens, (char *)s);
 		ft_expansor(env, tokens);
 		ft_executor(tokens, env, export, envp);
 		add_history(s);
 	//	free_tokens(tokens);
-=======
-		ft_quote(s);
-		tokens = get_tok(tokens, (char *)s);
-		ft_expansor(env, tokens);
-		builtins(tokens, export, env);
-		add_history(s);
-		free_tokens(tokens);
->>>>>>> 8b73d5e7bf7b0d94a37c6b66c27535a9d5bbf752:main.c
 	}
 	return (0);
 }
