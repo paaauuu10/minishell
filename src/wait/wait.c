698d38fcc844bcda1a0956496d9d919b1acec75d
Pau Botargues Julio <pbotargu@car13s6.42barcelona.com>
2024-07-05 11:41:04 +0200

    <norminette>src/wait/wait.c

--- src/wait/wait.c
+++ src/wait/wait.c
@@ -6,7 +6,7 @@
 /*   By: pbotargu <pbotargu@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/24 11:31:39 by pbotargu          #+#    #+#             */
-/*   Updated: 2024/07/03 14:40:10 by pbotargu         ###   ########.fr       */
+/*   Updated: 2024/07/05 11:39:41 by pbotargu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,81 +33,39 @@ void	ft_wait_one_child_process(void)
 	}
 }
 
-// Función para esperar a todos los procesos hijos
-void ft_wait_childs_process(int num_children, t_executor *t_exec)
+void	ft_aux_wait(int status)
 {
-    int status;
-    int exit_status;
+	int	term_signal;
 
-    for (int j = 0; j < num_children; j++)
-    {
-        pid_t pid = wait(&status); // Espera a cualquier proceso hijo
-
-        if (pid == -1)
-        {
-            perror("wait");
-            return;
-        }
-
-        if (WIFEXITED(status))
-        {
-            exit_status = WEXITSTATUS(status);
-            ft_exit_status(exit_status, 1);
-            //printf("Child %d exited with status %d\n", pid, exit_status);
-        }
-        else if (WIFSIGNALED(status))
-        {
-            int term_signal = WTERMSIG(status);
-            if (term_signal == SIGINT)
-            {
-                ft_exit_status(130, 1);
-                //printf("Child %d terminated by SIGINT\n", pid);
-            }
-            else if (term_signal == SIGQUIT)
-            {
-                ft_exit_status(131, 1);
-                //printf("Child %d terminated by SIGQUIT (Quit: 3)\n", pid);
-            }
-            else
-            {
-                ft_exit_status(128 + term_signal, 1);
-                //printf("Child %d terminated by signal %d\n", pid, term_signal);
-            }
-        }
-    }
-	(void)t_exec;
+	term_signal = WTERMSIG(status);
+	if (term_signal == SIGINT)
+		ft_exit_status(130, 1);
+	else if (term_signal == SIGQUIT)
+		ft_exit_status(131, 1);
+	else
+		ft_exit_status(128 + term_signal, 1);
 }
 
-
-/*void	ft_wait_childs_process(int i, t_executor *t_exec)
+void	ft_wait_childs_process(int num_children, t_executor *t_exec)
 {
-	int		j;
 	int		status;
-	int		k;
+	pid_t	pid;
+	int		j;
 
-	k = 0;
 	j = 0;
-	while (j < i)
+	while (j < num_children)
 	{
-		if (t_exec->pid == wait(&status))
+		pid = wait(&status);
+		if (pid == -1)
 		{
-			if (WIFEXITED(status))
-			{
-				k = WEXITSTATUS(status);
-				ft_exit_status(k, 1);
-				printf(" K: %d\n", k);
-			}
-			if (WIFSIGNALED(status))
-			{
-				if (WTERMSIG(status) == SIGINT)
-					ft_exit_status(130, 1);
-				else if (WTERMSIG(status) == SIGQUIT)
-				{
-					printf("Quit: 3\n");
-					ft_exit_status(131, 1);
-				}
-			}
+			perror("wait");
+			return ;
 		}
+		if (WIFEXITED(status))
+			ft_exit_status(WEXITSTATUS(status), 1);
+		else if (WIFSIGNALED(status))
+			ft_aux_wait(status);
 		j++;
 	}
-}*/
+	(void)t_exec;
+}
